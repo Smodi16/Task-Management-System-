da4c4db4a58ccf7bf2c802d181fa854501db1179
Smodi16 <34683300+Smodi16@users.noreply.github.com>
2020-11-08 22:11:45 +0530

    Delete keys_dev.js

--- server/config/keys_dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mongoURI: process.env.MONGO_URI || 'mongodb+srv://Smodi:Kevin123@cluster1-94nsb.mongodb.net/Test?retryWrites=true&w=majority',
-   // secretOrKey: process.env.SECRET_OR_KEY,
-    secretOrKey: 'secret',
-    cloudName: process.env.CLOUD_NAME,
-    apiKey: process.env.API_KEY,
-    apiSecret: process.env.API_SECRET,
-  };
-  
